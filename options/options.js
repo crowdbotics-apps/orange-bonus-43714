141a46771619b4f6035f6fa7bbf72a933831cbcd
Crowdbotics <team@crowdbotics.com>
2023-10-02 21:27:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-bonus-43714",
+  url: "https://orange-bonus-43714.botics.co",
+  api: "https://orange-bonus-43714.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
